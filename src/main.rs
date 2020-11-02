080ec55cf26a5b2d3eb14215e6601a8df2cb763a
cobyeast <cobyeastwood@gmail.com>
2020-11-01 19:46:28 -0800

    adding json support and foundation structs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
+// @date: Updated on Nov 6, 2020
+// @name: main.rs
+// @desc: server
+
 use actix_session::Session;
 use actix_web::http::StatusCode;
 use actix_web::{get, middleware, App, HttpRequest, HttpResponse, HttpServer, Result};
+use json::JsonValue;
+use serde::{Deserialize, Serialize};
+
+use bits::{Group};
+
+async fn index(item: web::Json<MyObj>) -> HttpResponse {
+    println!("{:?}", &item);
+    HttpResponse::Ok().json(item.0)
+}
 
 // Derive 'get' handler
 #[get("/home")]
 async fn home(session: Session, req: HttpRequest) -> Result<HttpResponse> {
-    println!("{:?}", req);
+    println!("Req: {:?}", req);
 
     let mut counter = 1;
 
@@ -26,8 +39,16 @@ async fn home(session: Session, req: HttpRequest) -> Result<HttpResponse> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().wrap(middleware::Logger::default()).service(home))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new().wrap(middleware::Logger::default())
+            .data(web::JsonConfig::default().limit(1024))
+            .service(web::resource("/api/v1/").route(web::post().to(index)))
+            .service(home)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
+
+
+// Json Documentation to Reference -> https://github.com/actix/examples/blob/master/json/src/main.rs
